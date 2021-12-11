c117af2534b8f451c457928d17ef5e980a4c3be9
tigerrex9 <33882494+tigerrex9@users.noreply.github.com>
2021-12-10 20:13:49 -0800

    added to README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Crossword-CLI
+This is (or more accurately is going to be) a program for playing crossword puzzles in the command line.
--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
 #include "main.h"
+#include <iostream>
+#include <ncurses.h>
 
 using namespace std;
 
 int main(){
+
     cout << "test\n";
 
     return 0;
