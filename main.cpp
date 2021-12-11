9b6073be10967299efce3739ebb6090bec9d61c4
tigerrex9 <33882494+tigerrex9@users.noreply.github.com>
2021-12-10 17:30:09 -0800

    added cmake to git ignore and added main files

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "main.h"
+
+using namespace std;
+
+int main(){
+    cout << "test\n";
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by manu on 12/10/21.
+//
+
+#ifndef CROSSWORD_CLI_MAIN_H
+#define CROSSWORD_CLI_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //CROSSWORD_CLI_MAIN_H
